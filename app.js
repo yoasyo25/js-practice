085bfd16724ec65a335b403e3c889c8179aa239a
Yohanan Assefa <nanahoy@gmail.com>
2017-10-23 13:35:42 -0600

    Sum all numbers in a range

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// Sum All Numbers in a Range
+/* Sum All Numbers in a Range
+We'll pass you an array of two numbers. Return the sum of those two numbers and
+all numbers between them.
+*/
+
+function sumAll(arr) {
+  var start = Math.min.apply(null, arr);
+  var end = Math.max.apply(null, arr);
+  var increment = 0;
+  var sum = 0;
+
+  for (var i = start; i <= end; i++) {
+    sum += (start + increment)
+    increment++;
+  }
+
+  return sum;
+}
