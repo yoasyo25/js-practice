8ffc8f226e739fbc516baf6cf38ec390916079fe
Yohanan Assefa <nanahoy@gmail.com>
2017-10-23 20:43:51 -0600

    Truncate a string

--- app.js
+++ app.js
@@ -103,3 +103,25 @@ function repeatStringNumTimes(str, num) {
 
   return repeatedString;
 }
+
+/* Truncate a string (first argument) if it is longer than the given maximum
+string length (second argument). Return the truncated string with a ... ending.
+Note that inserting the three dots to the end will add to the string length.
+However, if the given maximum string length num is less than or equal to 3,
+then the addition of the three dots does not add to the string length in
+determining the truncated string.
+*/
+
+function truncateString(str, num) {
+  var result = "";
+  if (num <= 3) {
+     result = str.slice(0, num).trim().concat("...");
+  } else if (str.length === num) {
+     return str;
+  } else if (str.length > num) {
+   result = str.slice(0, num - 2).trim().concat("...");
+ } else if (str.length < num) {
+   result = str.slice(0, num - 2).trim();
+ }
+  return result;
+}
